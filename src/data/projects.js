77d2a0ad934bdc1bf2c60123e44e025b5790f7b4
Yasir Faizan <yasirfaizan680@gmail.com>
2026-05-16 11:18:47 -0700

    Modify project list in projects.js
    Updated project entries in projects.js, adding new projects and modifying existing ones.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,54 @@
 export const projects = [
-  {
+ {
     id: 1,
+    title: "Aziz Jan Trust",
+    description: "The Real Website for the Famous Institute of KPK, which helps people and provides free Education.",
+    tags: ["React", "Tailwinds", "Vite", "Vercel"],
+    liveUrl: "https://aziz-jan-trust.vercel.app/",
+    githubUrl: "https://github.com/Yasirfaizan/Aziz-Jan-Trust",
+},
+{
+  id: 2,
+  title: "Peshawar Model School",
+  description: "A mock e-commerce platform featuring product listings, cart functionality, and checkout.",
+  tags: ["React", "Vite", "Tailwind CSS"],
+  liveUrl: "https://pmdc-school-react.vercel.app/",
+  githubUrl: "https://github.com/Yasirfaizan/PMDC-School-React"
+  },
+{
+  id: 3,
+  title: "Chain Forge",
+  description: "The Firebase for Web3 app help to connect wallets and features wallets transactions and generate API keys.",
+  tags: ["React", "Express", "MONGODB", "Railway"],
+  liveUrl: "https://chainforge-henna.vercel.app/",
+  githubUrl: "https://github.com/Yasirfaizan/chainforge"
+},
+{
+  id: 4,
+  title: "Tent and Catering Services",
+  description: "A Tent and Catering Website For Online Services via Whatsapp,Focus on client Demand and His Details.",
+  tags: ["React","CSS","Tailwind CSS","Smooth Animations"],
+  liveUrl: "https://shahab-tent-and-catering.vercel.app/",
+  githubUrl: "https://github.com/Yasirfaizan/Tent-Services"
+},
+  {
+    id: 5,
+    title: "Portfolio Design One",
+    description: "A mock portfolio website featuring a clean and modern design, with a focus on user experience.",
+    tags: ["React", "Lucide", "Vite","Tailwind CSS"],
+    liveUrl: "https://portfolio-design-1-react.vercel.app/",
+    githubUrl: "https://github.com/Yasirfaizan/Portfolio-Design-1-React"
+},
+{
+  id: 6,
+  title: "Portfolio Design Two",
+  description: "A mock portfolio website featuring a clean and modern design, with a focus on user experience.",
+  tags: ["React", "Vite", "Tailwind CSS"],
+  liveUrl: "https://portfolio-design-2-react.vercel.app/",
+  githubUrl: "https://github.com/Yasirfaizan/Portfolio-Design-2-React"
+},
+  {
+    id: 7,
     title: "School Project One",
     description: "A brief description of this amazing project and the technologies used to build it.",
     tags: ["React", "Lucide", "Vite"],
@@ -9,60 +57,13 @@ export const projects = [
     githubUrl: "https://github.com/Yasirfaizan/School-React"
   },
   {
-    id: 2,
-    title: "Portfolio Design One",
-    description: "A mock portfolio website featuring a clean and modern design, with a focus on user experience.",
-    tags: ["React", "Lucide", "Vite","Tailwind CSS"],
-    liveUrl: "https://portfolio-design-1-react.vercel.app/",
-    githubUrl: "https://github.com/Yasirfaizan/Portfolio-Design-1-React"
-},
-{
-    id: 3,
-    title: "Task Management Tool",
-    description: "A collaborative task management application for organizing your daily workflow.",
-    tags: ["React", "Vite", "Tailwind CSS"],
-    liveUrl: "https://to-do-list-js-bice.vercel.app/",
-    githubUrl: "https://github.com/Yasirfaizan/To-Do-List-JS"
-},
-{
-  id: 4,
-  title: "Weather App",
-  description: "A weather application that displays the current weather conditions for a given location.",
-  tags: ["React", "Vite", "Tailwind CSS"],
-  liveUrl: "https://weather-app-flame-omega-35.vercel.app/",
-  githubUrl: "https://github.com/Yasirfaizan/Weather-App"
-},
-{
-  id: 5,
-  title: "Portfolio Design Two",
-  description: "A mock portfolio website featuring a clean and modern design, with a focus on user experience.",
-  tags: ["React", "Vite", "Tailwind CSS"],
-  liveUrl: "https://portfolio-design-2-react.vercel.app/",
-  githubUrl: "https://github.com/Yasirfaizan/Portfolio-Design-2-React"
-},
-{
-  id: 6,
-  title: "Peshawar Model School",
-  description: "A mock e-commerce platform featuring product listings, cart functionality, and checkout.",
-  tags: ["React", "Vite", "Tailwind CSS"],
-  liveUrl: "https://pmdc-school-react.vercel.app/",
-  githubUrl: "https://github.com/Yasirfaizan/PMDC-School-React"
-},
-{
-  id:7,
+  
+  id:8,
   title: "Marketing Agency One",
   description: "A mock marketing agency website featuring a clean and modern design, with a focus on user experience.",
   tags: ["React", "Vite", "Tailwind CSS"],
   liveUrl: "https://marketing-agency-1-react.vercel.app/",
   githubUrl: "https://github.com/Yasirfaizan/Marketing-Agency-1-React"
-},
-{
-  id:8,
-  title: "Marketing Agency Two",
-  description: "A mock marketing agency website featuring a clean and modern design, with a focus on user experience.",
-  tags: ["React", "Vite", "Tailwind CSS"],
-  liveUrl: "https://marketing-agency-2-react.vercel.app/",
-  githubUrl: "https://github.com/Yasirfaizan/Marketing-Agency-2-React"
 },
 {
   id: 9,
@@ -74,35 +75,35 @@ export const projects = [
 },
 {
   id: 10,
-  title: "Tent and Catering Services",
-  description: "A Tent and Catering Website For Online Services via Whatsapp,Focus on client Demand and His Details.",
-  tags: ["React","CSS","Tailwind CSS","Smooth Animations"],
-  liveUrl: "https://shahab-tent-and-catering.vercel.app/",
-  githubUrl: "https://github.com/Yasirfaizan/Tent-Services"
-},
-{
-  id: 11,
-  title: "Chain Forge",
-  description: "The Firebase for Web3 app help to connect wallets and features wallets transactions and generate API keys.",
-  tags: ["React", "Express", "MONGODB", "Railway"],
-  liveUrl: "https://chainforge-henna.vercel.app/",
-  githubUrl: "https://github.com/Yasirfaizan/chainforge"
-},
-{
-  id: 12,
-  title: "Aziz Jan Trust",
-  description: "The Real Website for the Famous Institute of KPK, which helps people and provides free Education.",
-  tags: ["React", "Tailwinds", "Vite", "Vercel"],
-  liveUrl: "https://aziz-jan-trust.vercel.app/",
-  githubUrl: "https://github.com/Yasirfaizan/Aziz-Jan-Trust",
-},
-{
-  id: 13,
   title: "Ad Gate PHP",
   description: "A mock form login signup application featuring a clean and modern design, with a focus on user experience.",
   tags: ["PHP", "HTML", "CSS", "JavaScript","SQL"],
   liveUrl: "http://ad-gate.infinityfree.me/index.php",
   githubUrl: "https://github.com/Yasirfaizan/AD-Gate-PHP"
+},
+{
+  id:11,
+  title: "Weather App",
+  description: "A weather application that displays the current weather conditions for a given location.",
+  tags: ["React", "Vite", "Tailwind CSS"],
+  liveUrl: "https://weather-app-flame-omega-35.vercel.app/",
+  githubUrl: "https://github.com/Yasirfaizan/Weather-App"
+},
+{
+  id:12,
+  title: "Marketing Agency Two",
+  description: "A mock marketing agency website featuring a clean and modern design, with a focus on user experience.",
+  tags: ["React", "Vite", "Tailwind CSS"],
+  liveUrl: "https://marketing-agency-2-react.vercel.app/",
+  githubUrl: "https://github.com/Yasirfaizan/Marketing-Agency-2-React"
+},
+{
+    id: 13,
+    title: "Task Management Tool",
+    description: "A collaborative task management application for organizing your daily workflow.",
+    tags: ["React", "Vite", "Tailwind CSS"],
+    liveUrl: "https://to-do-list-js-bice.vercel.app/",
+    githubUrl: "https://github.com/Yasirfaizan/To-Do-List-JS"
 },
 ];
 
